fbd95401e6bee0ffbaebe2edaf9c3278b986ffc0
junho7108 <yyjh7108@gmail.com>
2024-10-02 13:15:06 +0900

    Set up initial Xcode project structure

---
+++ AllergyWallet/AllergyWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllergyWalletApp.swift
+//  AllergyWallet
+//
+//  Created by Junho Yoon on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AllergyWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
